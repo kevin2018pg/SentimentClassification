b0802e41dcf40e4c317c6b749be5816055e7e145
Kevin 13 <kevin2018pg@163.com>
2021-04-30 14:46:00 +0800

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # usual
-python roberta_k_fold.py \
-	--pre_train_path roberta_large \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/roberta_large \
 	--model_name BertBase \
-	--output_dir usual_k_fold_model/roberta_large \
+	--output_dir submission/roberta_large \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -17,10 +17,10 @@ python roberta_k_fold.py \
 	--k_fold 5 \
 	--do_train
 
-python roberta_k_fold.py \
-	--pre_train_path roberta_base \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/roberta_base \
 	--model_name BertBase \
-	--output_dir usual_k_fold_model/roberta_base \
+	--output_dir submission/roberta_base \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -34,10 +34,10 @@ python roberta_k_fold.py \
 	--k_fold 5 \
 	--do_train
 
-python roberta_k_fold.py \
-	--pre_train_path roberta_wwm_ext_large \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/roberta_wwm_ext_large \
 	--model_name BertBase \
-	--output_dir usual_k_fold_model/roberta_wwm_ext_large \
+	--output_dir submission/roberta_wwm_ext_large \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -51,10 +51,10 @@ python roberta_k_fold.py \
 	--k_fold 5 \
 	--do_train
 
-python roberta_k_fold.py \
-	--pre_train_path roberta_large \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/roberta_large \
 	--model_name BertLSTMAttention \
-	--output_dir usual_k_fold_model/roberta_large_lstm_attention \
+	--output_dir submission/roberta_large_lstm_attention \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -68,10 +68,10 @@ python roberta_k_fold.py \
 	--k_fold 5 \
 	--do_train
 
-python roberta_k_fold.py \
-	--pre_train_path roberta_large \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/roberta_large \
 	--model_name BertAttention \
-	--output_dir usual_k_fold_model/roberta_large_attention \
+	--output_dir submission/roberta_large_attention \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -85,10 +85,10 @@ python roberta_k_fold.py \
 	--k_fold 5 \
 	--do_train
 
-python roberta_k_fold.py \
-	--pre_train_path uer_large \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/uer_large \
 	--model_name BertUER \
-	--output_dir usual_k_fold_model/uer_large \
+	--output_dir submission/uer_large \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -102,10 +102,10 @@ python roberta_k_fold.py \
 	--k_fold 5 \
 	--do_train
 
-python roberta_k_fold.py \
-	--pre_train_path virus_k_fold_model/roberta_wwm_ext \
+python k_fold_bert_variants_model.py \
+	--pre_train_path pretrained_model/roberta_wwm_ext \
 	--model_name BertTransferLearning \
-	--output_dir usual_k_fold_model/roberta_wwm_ext_transfer_learning1 \
+	--output_dir submission/roberta_wwm_ext_transfer_learning1 \
 	--data_dir k_fold/usual \
 	--max_seq_length 140 \
 	--train_batch_size 8 \
@@ -117,58 +117,4 @@ python roberta_k_fold.py \
 	--attack fgm \
 	--log_dir roberta_wwm_ext_transfer_learning1.log \
 	--k_fold 5 \
-	--do_train
-
-
-	# virus
-	python roberta_k_fold.py \
-	--pre_train_path roberta_large \
-	--model_name BertBase \
-	--output_dir virus_k_fold_model/roberta_large \
-	--data_dir k_fold/virus \
-	--max_seq_length 140 \
-	--train_batch_size 8 \
-	--gradient_accumulation_steps 8 \
-	--num_train_epochs 20 \
-	--learning_rate 1e-5 \
-	--warmup_rate 0.3 \
-	--weight_decay 0.02 \
-	--attack fgm \
-	--log_dir roberta_large.log \
-	--k_fold 5 \
-	--do_train
-
-	python roberta_k_fold.py \
-	--pre_train_path usual_k_fold_model/roberta_wwm_ext \
-	--model_name BertTransferLearning \
-	--output_dir virus_k_fold_model/roberta_wwm_ext_transfer_learning \
-	--data_dir k_fold/virus \
-	--max_seq_length 140 \
-	--train_batch_size 8 \
-	--gradient_accumulation_steps 8 \
-	--num_train_epochs 20 \
-	--learning_rate 1e-5 \
-	--warmup_rate 0.3 \
-	--weight_decay 0.02 \
-	--attack fgm \
-	--log_dir roberta_wwm_ext_transfer_learning.log \
-	--k_fold 5 \
-	--do_train
-
-	python roberta_k_fold.py \
-	--pre_train_path usual_k_fold_model/roberta_wwm_ext \
-	--model_name BertTransferLearning \
-	--output_dir virus_k_fold_model/roberta_wwm_ext_transfer_learning_pseudo \
-	--data_dir k_fold/virus \
-	--max_seq_length 140 \
-	--train_batch_size 8 \
-	--gradient_accumulation_steps 8 \
-	--num_train_epochs 20 \
-	--learning_rate 1e-5 \
-	--warmup_rate 0.3 \
-	--weight_decay 0.02 \
-	--attack fgm \
-	--log_dir roberta_wwm_ext_transfer_learning_pseudo.log \
-	--k_fold 5 \
-	--use_pseudo_data pseudo \
 	--do_train
